573f18d7ee4e13b387d6a9ba9950efe51c43eac1
sumalinogcks <css9223@students.uc-bcf.edu.ph>
2025-03-19 22:15:03 +0800

    Added abstract

--- src/Entity.java
+++ src/Entity.java
@@ -1,4 +1,42 @@
 
 public class Entity {
+    
+    private String name;
+    private String email;
+    private int id;
+    private int contactNo;
+
+
+    public void enroll(){
+
+    }
+    
+    public void setName(String name){
+
+    }
+
+    public void setId(){
+
+    }
+
+    public void setContactNo(){
+
+    }
+
+    public void setEmail(){
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getEmail(){
+        return email;
+    }
 
 }
